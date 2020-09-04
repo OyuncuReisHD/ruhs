41d23b419a44fc20404b6fa9e86bb39410a4ac49
croxy <44734178+CroxyTheDev@users.noreply.github.com>
2020-09-04 21:18:01 +0300

    🎨 Removed async.

--- src/methods/pinMessage.js
+++ src/methods/pinMessage.js
@@ -1,5 +1,5 @@
 const request = require("../utils/request.js");
 
-module.exports = async function(channelID, messageID) {
-	await request("PUT", "/channels/" + channelID + "/pins/" + messageID);
+module.exports = function(channelID, messageID) {
+  request("PUT", "/channels/" + channelID + "/pins/" + messageID);
 }
