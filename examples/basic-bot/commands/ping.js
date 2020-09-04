badfe25e6b46a9a519e3d76866afc8fedfb80b28
cembkoztekin <59697324+cembkoztekin@users.noreply.github.com>
2020-09-04 13:44:28 +0300

    Update ping.js

--- examples/basic-bot/commands/ping.js
+++ examples/basic-bot/commands/ping.js
@@ -2,7 +2,7 @@ const ruhs = require("ruhs"); // Import ruhs module
 
 module.exports = { // Create command export 
 	name: 'ping', // Add command name
-	execute(message, args) { // Execute message and args
+	execute(message) { // Execute message and args
 		ruhs.sendMessage(message.channel.id, "Pong!") // Send "Pong!" message
 	},
 };
