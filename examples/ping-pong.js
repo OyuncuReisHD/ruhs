d0618846f770a348c23367f3e33891a9eac9b9fa
cembkoztekin <59697324+cembkoztekin@users.noreply.github.com>
2020-09-04 12:49:07 +0300

    Created ping-pong example in ruhs

--- examples/ping-pong.js
+++ examples/ping-pong.js
@@ -1,1 +1,14 @@
+const ruhs = require("ruhs");
+const token = "TOKEN";
 
+ruhs.eventHandlers.messageCreate = ((message) => {
+  if (message.content === "ping") {
+      ruhs.sendMessage(message.channel().id, "Pong!")
+  }
+});
+
+(async () => {
+  await ruhs.createClient(token, {
+    "intents": "ALL"
+  });
+})();
