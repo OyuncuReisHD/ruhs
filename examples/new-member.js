8fb03b32d4c9c9171a1911135a76f7a8cc558b6c
cembkoztekin <59697324+cembkoztekin@users.noreply.github.com>
2020-09-04 12:58:21 +0300

    Update new-member.js

--- examples/new-member.js
+++ examples/new-member.js
@@ -1,5 +1,5 @@
 const ruhs = require("ruhs"); // Import ruhs module
-const token = "NzUwNjUzODIzMzM2MzE2OTU5.X09q2Q.kbkUuIJ8F6ip5s6lrvUijeYQO6Y"; // Enter your DISCORD bot app token
+const token = "TOKEN"; // Enter your DISCORD bot app token
 
 ruhs.eventHandlers.guildMemberAdd = ((member) => { // Create the guildMemberAdd event
     ruhs.sendMessage("750728553569452103", `Welcome ${member.user.tag}`) // Send message a "Welcome User#0000" to the specified channel ID
