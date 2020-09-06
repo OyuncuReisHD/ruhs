4248dfc2c7a3b15f5ccc06742bc36d4a551092d0
croxy the developer <discordcroxy2@gmail.com>
2020-09-07 02:56:19 +0300

    ✨ Added 'giveRole' and 'removeRole' methods. Also, added 'getInvites' method & structure.

---
+++ src/structures/createInvite.js
@@ -0,0 +1,31 @@
+const {cache} = require("../botProperties.js");
+const request = require("../utils/request.js")
+const createUser = require("./createUser.js");
+
+const createAuditLog = (async(data) => {
+
+	const d = {};
+
+	data = data[0]
+
+	d.guild = cache.guilds.get(data.guild.id);
+
+	if(data.inviter) {
+		var req = await request("GET", "/users/"+data.inviter.id)
+		d.inviter = createUser(req);
+	}
+
+	if(data.channel) {
+		d.channel = cache.channels.get(data.channel.id);
+	}
+
+	d.uses = data.uses;
+	d.max_uses = data.max_uses;
+	d.max_age = data.max_age;
+	d.temporary = data.temporary;
+	d.createdAt = data.created_at;
+
+	return d;
+});
+
+module.exports = createAuditLog;
