3e74ffb4e6f78fb53640e282b30f4997221cefe4
acarnd03 <69324943+acarnd03@users.noreply.github.com>
2020-09-07 00:35:04 +0300

    :fire: Removed unused methods.

--- src/structures/createChannel.js
+++ src/structures/createChannel.js
@@ -1,7 +1,5 @@
 const createChannel = ((channelData) => {
   const createUser = require("./createUser");
-  const sendMessage = require("../methods/sendMessage.js");
-  const deleteChannel = require("../methods/deleteChannel.js");
 
   const channel = {};
   const types = ["text", "dm", "voice", "group_dm", "category", "news", "guild_store"];
